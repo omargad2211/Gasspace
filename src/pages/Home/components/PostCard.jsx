89160a7efe902bd6e73db33d8ad1bbc85909da2e
omar gad <152779410+omargad2211@users.noreply.github.com>
2025-02-14 00:09:10 +0200

    feat: add post card component

---
+++ src/pages/Home/components/PostCard.jsx
@@ -0,0 +1,45 @@
+import { AiOutlineMessage } from "react-icons/ai";
+import { FaRegBookmark, FaRegHeart } from "react-icons/fa6";
+import { RiShareForwardLine } from "react-icons/ri";
+
+const PostCard = () => {
+  return (
+    <div className="bg-white p-2 mt-2">
+      <div className="flex justify-start items-center gap-2">
+        <img
+          src="public\images\User-Profile-PNG-Clipart.png"
+          alt="profile"
+          className="size-8 rounded-full"
+        />
+        <div className="flex flex-col ">
+          <p className="text-black text-sm font-semibold">full name</p>
+          <p className="text-gray-500 text-xs">3 Aug at 4:21 pm</p>
+        </div>
+      </div>
+      <img
+        src="http://wallup.net/wp-content/uploads/2016/03/10/318220-photography-nature-water-landscape-lake-plants-trees.jpg"
+        alt=""
+        className="w-full rounded-lg mt-4"
+      />
+      <div className="w-full border-t boder-2 mt-3 p-2 text-gray-500  flex justify-between items-center gap-4 text-sm md:text-base">
+        <div className=" flex justify-start items-center gap-4">
+          <div className="flex items-center justify-center gap-1">
+            <FaRegHeart />
+            <p>32 Likes</p>
+          </div>
+          <div className="flex items-center justify-center gap-1">
+            <AiOutlineMessage />
+            <p>3 comments</p>
+          </div>{" "}
+          <div className="flex items-center justify-center gap-1">
+            <RiShareForwardLine />
+            <p>32 reposts</p>
+          </div>
+        </div>
+        <FaRegBookmark className="" />
+      </div>
+    </div>
+  );
+};
+
+export default PostCard;
