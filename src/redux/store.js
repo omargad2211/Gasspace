d2c2a8c59defcff4dad76ecc9d9213b8f0e1f4db
omar gad <152779410+omargad2211@users.noreply.github.com>
2025-02-14 12:21:49 +0200

    feat: implement auth and auth api in store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,15 @@
 // src/redux/store.js
 import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "./authSlice";
 import { postsApi } from "./postsApi";
+import { authApi } from "./authApi";
 
 export const store = configureStore({
   reducer: {
+    auth: authReducer,
     [postsApi.reducerPath]: postsApi.reducer,
+    [authApi.reducerPath]: authApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(postsApi.middleware),
+    getDefaultMiddleware().concat(postsApi.middleware, authApi.middleware),
 });
