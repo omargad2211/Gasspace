9b595873fef664f23ee623fc0751d6129d7a30a2
omar gad <152779410+omargad2211@users.noreply.github.com>
2025-02-14 12:22:43 +0200

    feat: implement updates in main and app js

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import PublicLayout from "./Layouts/PublicLayout";
 import Home from "./pages/Home";
+import SignUpForm from "./pages/register/SignUp";
 
 function App() {
   return (
     <>
       <Routes>
+        <Route path="/register" element={<SignUpForm />} />
         <Route element={<PublicLayout />}>
           <Route path="/" element={<Home />} />
         </Route>
--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,9 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { store } from "./redux/store.js";
 import { Provider } from "react-redux";
+import { authListener } from "./redux/authSlice.js";
 
+store.dispatch(authListener());
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <BrowserRouter>
