febd23530a7e4f75a80f701c10ae162c93b92503
omar gad <152779410+omargad2211@users.noreply.github.com>
2025-02-16 00:49:43 +0200

    feat: implement commentsApi and likesApi slice in the store

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,13 +3,22 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./authSlice";
 import { postsApi } from "./postsApi";
 import { authApi } from "./authApi";
+import { commentsApi } from "./commentsApi";
+import { likesApi } from "./likesApi";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     [postsApi.reducerPath]: postsApi.reducer,
     [authApi.reducerPath]: authApi.reducer,
+    [commentsApi.reducerPath]: commentsApi.reducer,
+    [likesApi.reducerPath]: likesApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(postsApi.middleware, authApi.middleware),
+    getDefaultMiddleware().concat(
+      postsApi.middleware,
+      authApi.middleware,
+      commentsApi.middleware,
+      likesApi.middleware
+    ),
 });
