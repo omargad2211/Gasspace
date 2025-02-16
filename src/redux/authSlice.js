4ddd6229848a4f3fe08919b7a91adfb9485f5bc7
omar gad <152779410+omargad2211@users.noreply.github.com>
2025-02-16 19:39:06 +0200

    feat: add updateUser hook

--- src/redux/authSlice.js
+++ src/redux/authSlice.js
@@ -1,7 +1,8 @@
 // src/redux/authSlice.js
 import { createSlice } from "@reduxjs/toolkit";
 import { onAuthStateChanged } from "firebase/auth";
-import { auth } from "../firebase/firbase";
+import { doc, getDoc } from "firebase/firestore";
+import { auth, db } from "../firebase/firbase";
 
 const authSlice = createSlice({
   name: "auth",
@@ -19,26 +20,72 @@ const authSlice = createSlice({
 
 export const { setUser } = authSlice.actions;
 
+// export const authListener = () => (dispatch) => {
+//   onAuthStateChanged(auth, async (user) => {
+//     if (user) {
+//       // Wait a bit to allow Firebase to update the photoURL
+//       await user.reload();
+//       const refreshedUser = auth.currentUser;
+
+//       dispatch(
+//         setUser({
+//           uid: refreshedUser.uid,
+//           email: refreshedUser.email,
+//           displayName: refreshedUser.displayName,
+//           photoURL: refreshedUser.photoURL, // Ensure updated photoURL
+//           bioText: refreshedUser.bioText || "",
+//           location: refreshedUser.location || "",
+//           headerImage: refreshedUser.headerImage || "",
+//         })
+//       );
+//     } else {
+//       dispatch(setUser(null));
+//     }
+//   });
+// };
+
 export const authListener = () => (dispatch) => {
   onAuthStateChanged(auth, async (user) => {
     if (user) {
-      // Wait a bit to allow Firebase to update the photoURL
+      // Reload to get latest data
       await user.reload();
       const refreshedUser = auth.currentUser;
 
-      dispatch(
-        setUser({
-          uid: refreshedUser.uid,
-          email: refreshedUser.email,
-          displayName: refreshedUser.displayName,
-          photoURL: refreshedUser.photoURL, // Ensure updated photoURL
-        })
-      );
+      // Fetch additional user details from Firestore
+      const userDoc = await getDoc(doc(db, "users", refreshedUser.uid));
+
+      if (userDoc.exists()) {
+        const userData = userDoc.data();
+
+        dispatch(
+          setUser({
+            uid: refreshedUser.uid,
+            email: refreshedUser.email,
+            displayName: refreshedUser.displayName,
+            photoURL: refreshedUser.photoURL,
+            bioText: userData.bioText || "",
+            headerImage: userData.headerImage || "",
+            location: userData.location || "",
+          })
+        );
+      }
     } else {
       dispatch(setUser(null));
     }
   });
 };
-
+export const updateUser = (user) => (dispatch) => {
+  dispatch(
+    setUser({
+      uid: user.uid,
+      email: user.email,
+      displayName: user.displayName,
+      photoURL: user.photoURL,
+      bioText: user.bioText || "",
+      location: user.location || "",
+      headerImage: user.headerImage || "",
+    })
+  );
+};
 
 export default authSlice.reducer;
