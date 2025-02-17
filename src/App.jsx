868a63adebfdcd63203231d494c3eb600da9e310
omar gad <152779410+omargad2211@users.noreply.github.com>
2025-02-17 21:37:44 +0200

    feat: add users profile route

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import { useDispatch } from "react-redux";
 import { useEffect } from "react";
 import { authListener } from "./redux/authSlice";
 import Login from "./pages/register/login";
+import UserProfile from "./pages/users";
 
 function App() {
   const dispatch = useDispatch();
@@ -22,6 +23,7 @@ function App() {
         <Route element={<PublicLayout />}>
           <Route path="/" element={<Home />} />
           <Route path="/profile" element={<Profile />} />
+          <Route path="/profile/:id" element={<UserProfile />} />
         </Route>
       </Routes>
     </>
--- src/components/LeftSidebar.jsx
+++ src/components/LeftSidebar.jsx
@@ -8,13 +8,14 @@ const LeftSidebar = () => {
   return (
     <div className="bg-primary fixed right-0 top-20 md:top-24 hidden md:flex flex-col justify-start items-start gap-4 px-2  h-screen w-1/5">
       <p className="text-base font-semibold">People you may know </p>
-      <div className="bg-white h-full md:h-1/2 flex flex-col items-start justify-around px-2 md:px-8 gap-8 md:w-full rounded-lg overflow-y-auto overflow-x-hidden scrollbar-hide pt-3">
+      <div className="bg-white h-full md:h-1/2 flex flex-col items-start justify-around px-2 gap-8 md:w-full rounded-lg overflow-y-auto overflow-x-hidden scrollbar-hide pt-3">
         {users
           ?.filter((user) => user.displayName && user.photoURL) // Filter valid users
           .map((user) => (
             <Link
               key={user.id}
-              className="flex justify-start items-center gap-2 text-gray-600 hover:bg-gray-600/50 px-2 py-1 w-full rounded-xl hover:text-white"
+              to={`/profile/${user.id}`}
+              className="flex justify-start items-center gap-2 text-gray-600 hover:bg-[#D9F8FF] px-2 py-1 w-full rounded-xl hover:text-blue-700"
             >
               <img
                 src={user.photoURL}
