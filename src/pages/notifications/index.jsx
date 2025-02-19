907e9b7cb4dc6bbf019262c2c6e127eb4477a752
omar gad <152779410+omargad2211@users.noreply.github.com>
2025-02-19 11:54:27 +0200

    style: fix handleVisitPost  and mark as seen

--- src/pages/notifications/index.jsx
+++ src/pages/notifications/index.jsx
@@ -1,11 +1,12 @@
 import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import {
   useGetNotificationsQuery,
   useMarkAsSeenMutation,
 } from "../../redux/notificationsApi";
 
 const Notifications = () => {
+  const navigate = useNavigate();
   const { currentUser } = useSelector((state) => state.auth);
   const { data: notifications = [], isLoading } = useGetNotificationsQuery(
     currentUser?.uid
@@ -17,7 +18,12 @@ const Notifications = () => {
   const handleMarkAsSeen = async (notifId) => {
     await markAsSeen({ userId: currentUser.uid, notificationId: notifId });
   };
-  console.log(notifications);
+
+  const handleVisitPost = (postId, notifId) => {
+    navigate(`/post/${postId}`);
+    handleMarkAsSeen(notifId);
+  };
+
   return (
     <div className="min-h-screen bg-primary px-4 mx-auto pt-24 pl-[100px] ss:pl-[150px] md:px-[20%]">
       <h2 className="text-lg font-bold mb-2">Notifications</h2>
@@ -31,108 +37,57 @@ const Notifications = () => {
               notif.seen ? "bg-gray-100" : "bg-blue-100"
             }`}
           >
-            <p>
-              {notif.type === "follow" && (
-                <span>
-                  <Link
-                    to={`/profile/${notif.fromUserId}`}
-                    className="font-bold flex justify-start items-center gap-1"
-                  >
-                    <img
-                      src={
-                        notif?.photoURL || "images/User-Profile-PNG-Clipart.png"
-                      }
-                      alt="profile"
-                      className="size-8 rounded-full"
-                    />
-                    <p className=" text-nowrap">
-                      {notif?.displayName || "User Name"}
-                    </p>
-                    {notif.fromUserId}
-                  </Link>{" "}
-                  followed you.
-                </span>
-              )}
-              {notif.type === "like" && (
-                <span className=" flex justify-start items-center gap-2 md:gap-4">
-                  <Link
-                    to={`/profile/${notif.fromUserId}`}
-                    className="font-bold flex justify-start items-center gap-1"
-                  >
-                    <img
-                      src={
-                        notif?.photoURL || "images/User-Profile-PNG-Clipart.png"
-                      }
-                      alt="profile"
-                      className="size-8 rounded-full"
-                    />
-                    <p className=" text-nowrap">
-                      {notif?.displayName || "User Name"}
-                    </p>
-                  </Link>{" "}
-                  liked your{" "}
-                  <Link to={`/post/${notif.postId}`} className="text-blue-500">
-                    post
-                  </Link>
-                  .
-                </span>
-              )}
-              {notif.type === "comment" && (
-                <span className=" flex justify-start items-center gap-2 md:gap-4">
-                  <Link
-                    to={`/profile/${notif.fromUserId}`}
-                    className="font-bold flex justify-start items-center gap-1"
-                  >
-                    <img
-                      src={
-                        notif?.photoURL || "images/User-Profile-PNG-Clipart.png"
-                      }
-                      alt="profile"
-                      className="size-8 rounded-full"
-                    />
-                    <p className="">
-                      {notif?.displayName || "User Name"}
-                    </p>{" "}
-             
-                  </Link>{" "}
-                  commented on your{" "}
-                  <Link to={`/post/${notif.postId}`} className="text-blue-500">
-                    post
-                  </Link>
-                  .
-                </span>
-              )}
-              {notif.type === "repost" && (
-                <span className=" flex justify-start items-center gap-2 md:gap-4">
-                  <Link
-                    to={`/profile/${notif.fromUserId}`}
-                    className="font-bold flex justify-start items-center gap-1"
-                  >
-                    <img
-                      src={
-                        notif?.photoURL || "images/User-Profile-PNG-Clipart.png"
-                      }
-                      alt="profile"
-                      className="size-8 rounded-full"
-                    />
-                    <p className=" text-nowrap">
-                      {notif?.displayName || "User Name"}
-                    </p>
-                  </Link>{" "}
-                  reposted your{" "}
-                  <Link to={`/post/${notif.postId}`} className="text-blue-500">
-                    post
-                  </Link>
-                  .
-                </span>
-              )}
-            </p>
-            {!notif.seen && (
-              <button
+            {notif.type === "follow" && (
+              <div
                 onClick={() => handleMarkAsSeen(notif.id)}
-                className="text-sm text-gray-600"
+                className="flex items-center gap-2"
               >
-                Mark as read
+                <Link
+                  to={`/profile/${notif.fromUserId}`}
+                  onClick={(e) => e.stopPropagation()}
+                  className="font-bold flex justify-start items-center gap-1"
+                >
+                  <img
+                    src={
+                      notif?.photoURL || "images/User-Profile-PNG-Clipart.png"
+                    }
+                    alt="profile"
+                    className="size-8 rounded-full"
+                  />
+                  <p className="text-nowrap">
+                    {notif?.displayName || "User Name"}
+                  </p>
+                </Link>
+                <span>followed you.</span>
+              </div>
+            )}
+
+            {["like", "comment", "repost"].includes(notif.type) && (
+              <button
+                onClick={() => handleVisitPost(notif.postId, notif.id)}
+                className="flex justify-start items-center gap-2 md:gap-4 w-full text-left text-[9px] xs:text-[13px] md:text-lg"
+              >
+                <Link
+                  to={`/profile/${notif.fromUserId}`}
+                  onClick={(e) => e.stopPropagation()}
+                  className="font-bold flex justify-start items-center gap-1"
+                >
+                  <img
+                    src={
+                      notif?.photoURL || "images/User-Profile-PNG-Clipart.png"
+                    }
+                    alt="profile"
+                    className="size-8 rounded-full"
+                  />
+                  <p className="text-nowrap">
+                    {notif?.displayName || "User Name"}
+                  </p>
+                </Link>
+                {notif.type === "like" && <span>liked your post</span>}
+                {notif.type === "comment" && (
+                  <span>commented on your post</span>
+                )}
+                {notif.type === "repost" && <span>reposted your post.</span>}
               </button>
             )}
           </div>
