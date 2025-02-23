aa1619d4660866b4516fa121737fa67336c71007
omar gad <152779410+omargad2211@users.noreply.github.com>
2025-02-23 11:44:38 +0200

    feat: add saved posts layout

--- src/pages/profile/components/SavedPosts.jsx
+++ src/pages/profile/components/SavedPosts.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import PostCard from "../../Home/components/PostCard";
 
 const SavedPosts = () => {
+  const savedPosts = useSelector((state) => state.saved.items);
+  const dispatch = useDispatch();
+  console.log(savedPosts);
   return (
-    <div>
-      saved
+    <div className="space-y-4">
+      {savedPosts.length === 0 ? (
+        <p className="text-center text-gray-500">No posts yet.</p>
+      ) : (
+        savedPosts.map((post) => (
+          <div key={post.id}>
+            <PostCard post={post} />
+          </div>
+        ))
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default SavedPosts
+export default SavedPosts;
