d2f7ce262a5c0f8d039076a1ebb32e3a691b4a41
omar gad <152779410+omargad2211@users.noreply.github.com>
2025-02-25 15:53:58 +0200

    fix: fix home loading

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -7,7 +7,13 @@ const Home = () => {
   const { data: posts, error, isLoading } = useGetPostsQuery();
 
   if (isLoading) {
-    return [...Array(4)].map((_, index) => <PostCardSkeleton key={index} />);
+    return (
+      <div className="min-h-screen bg-primary px-4 mx-auto pt-24 pl-[100px] ss:pl-[150px]  md:px-[20%]">
+        {[...Array(4)].map((_, index) => (
+          <PostCardSkeleton key={index} />
+        ))}
+      </div>
+    );
   }
   return (
     <div className="min-h-screen bg-primary px-4 mx-auto pt-24 pl-[100px] ss:pl-[150px]  md:px-[20%]">
