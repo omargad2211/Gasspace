67325be2bcc3a98d39e9eea676ee09331beeeea1
omar gad <152779410+omargad2211@users.noreply.github.com>
2025-02-25 15:47:06 +0200

    feat: implement loading case in home screen

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,14 @@
 import { useGetPostsQuery } from "../../redux/postsApi";
+import PostCardSkeleton from "../Skeletons/PostCardSkeleton";
 import AddPost from "./components/AddPost";
 import PostCard from "./components/PostCard";
 
 const Home = () => {
   const { data: posts, error, isLoading } = useGetPostsQuery();
 
-
+  if (isLoading) {
+    return [...Array(4)].map((_, index) => <PostCardSkeleton key={index} />);
+  }
   return (
     <div className="min-h-screen bg-primary px-4 mx-auto pt-24 pl-[100px] ss:pl-[150px]  md:px-[20%]">
       <AddPost />
